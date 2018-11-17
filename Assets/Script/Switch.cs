922b6285743f77ce301d7b23baed1d770504b2e3
Unknown <andrea3.0@hotmail.it>
2018-11-17 17:48:03 +0100

    Scena di test + prime modifiche scripts

---
+++ Assets/Script/Switch.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+public class Switch : MonoBehaviour {
+    public GameObject Block;
+	// Use this for initialization
+	void Start () {
+        AudioListener.volume = 1f;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+    public void NoAudio()
+    {
+        AudioListener.volume = 0f;
+        Block.gameObject.SetActive(false);
+    }
+    public void NoVideo()
+    {
+        AudioListener.volume = 1f;
+        Block.gameObject.SetActive(true);
+    }
+}
